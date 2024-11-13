1a8759816867875530b40097a46f088d55c66014
Michael-porato <mporato05@gmail.com>
2024-11-13 18:48:57 -0500

    updated variable name

--- src/main/Interface/Main.java
+++ src/main/Interface/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner var7 = new Scanner(System.in);
+        Scanner keyIn = new Scanner(System.in);
         boolean var8 = false;
 
         String var1 = "clientUser";      // Placeholder client username
@@ -24,25 +24,26 @@ public class Main {
             System.out.println("4. Exit");
             System.out.print("Choose an option: ");
             
-            int var9 = var7.nextInt();
-            var7.nextLine(); // Consume newline
+            int choice1 = keyIn.nextInt();
+            keyIn.nextLine(); // Consume newline
             
-            if (var9 == 1) {
+            if (choice1 == 1) {
                 System.out.println("Login Menu:");
                 System.out.println("1. Login as Client");
                 System.out.println("2. Login as Administrator");
                 System.out.println("3. Login as Instructor");
                 System.out.print("Choose your role to log in: ");
                 
-                int var13 = var7.nextInt();
-                var7.nextLine(); // Consume newline
+                int choice2 = keyIn.nextInt();
+                keyIn.nextLine(); // Consume newline
+                
                 String var11, var12;
 
-                if (var13 == 1) {
+                if (choice2 == 1) {
                     System.out.print("Enter Client username: ");
-                    var11 = var7.nextLine();
+                    var11 = keyIn.nextLine();
                     System.out.print("Enter Client password: ");
-                    var12 = var7.nextLine();
+                    var12 = keyIn.nextLine();
 
                     if (var11.equals(var1) && var12.equals(var2)) {
                         System.out.println("Client login successful!");
@@ -50,11 +51,11 @@ public class Main {
                         System.out.println("Incorrect username or password for Client.");
                     }
 
-                } else if (var13 == 2) {
+                } else if (choice2 == 2) {
                     System.out.print("Enter Administrator username: ");
-                    var11 = var7.nextLine();
+                    var11 = keyIn.nextLine();
                     System.out.print("Enter Administrator password: ");
-                    var12 = var7.nextLine();
+                    var12 = keyIn.nextLine();
 
                     if (var11.equals(var3) && var12.equals(var4)) {
                         System.out.println("Administrator login successful!");
@@ -62,11 +63,11 @@ public class Main {
                         System.out.println("Incorrect username or password for Administrator.");
                     }
 
-                } else if (var13 == 3) {
+                } else if (choice2 == 3) {
                     System.out.print("Enter Instructor username: ");
-                    var11 = var7.nextLine();
+                    var11 = keyIn.nextLine();
                     System.out.print("Enter Instructor password: ");
-                    var12 = var7.nextLine();
+                    var12 = keyIn.nextLine();
 
                     if (var11.equals(var5) && var12.equals(var6)) {
                         System.out.println("Instructor login successful!");
@@ -78,18 +79,18 @@ public class Main {
                     System.out.println("Invalid choice. Returning to the main menu.");
                 }
 
-            } else if (var9 == 2) {
+            } else if (choice1 == 2) {
                 System.out.print("Enter a username for the new Client: ");
-                String var10 = var7.nextLine();
+                String var10 = keyIn.nextLine();
                 System.out.print("Enter a password for the new Client: ");
-                var10 = var7.nextLine();
+                var10 = keyIn.nextLine();
                 System.out.println("Account created successfully as Client!");
 
-            } else if (var9 == 3) {
+            } else if (choice1 == 3) {
                 System.out.println("Viewing available offerings...");
                 // Logic to view offerings goes here
 
-            } else if (var9 == 4) {
+            } else if (choice1 == 4) {
                 var8 = true;
                 System.out.println("Exiting the system.");
 
@@ -98,7 +99,7 @@ public class Main {
             }
         }
 
-        var7.close();
+        keyIn.close();
     }
 }
 
